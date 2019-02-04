4eaa58e50c79cb935c21f853ad039702d0d3d261
v.polyanskiy <v.polyanskiy@simcord.com>
2019-02-04 10:48:44 +0200

    Removed comment

--- src/api/github/search/index.js
+++ src/api/github/search/index.js
@@ -2,10 +2,6 @@ import {
   callApi
 } from "api/ajax";
 
-/**
- * @returns Promise
- * @
- */
 export const repositories = () => callApi('search/repositories?q=+language:js&sort=stars&order=desc&page=1&per_page=10')
 
 export default {
